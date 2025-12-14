14f7d8be33e3d2a0d9b09fcd6b339e513d7e3ffd
Peter Forgacs <forgacspeter940@gmail.com>
2025-12-14 10:53:23 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/lernify/lernify_backend/LernifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lernify.lernify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lernify/lernify_backend/LernifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lernify.lernify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
